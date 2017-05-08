90f5e3846e65a0f4961ba2805e7038f82fea7fed
thanel@redletra.com <thanel@redletra.com>
2017-05-08 19:50:47 +1000

    error message css and animate

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 (function(){
-    var module = angular.module("lostProperty",["ngComponentRouter", "toaster",'ui.bootstrap']);
+    var module = angular.module("lostProperty",["ngComponentRouter", "toaster",'ui.bootstrap','ngAnimate','ngMessages']);
 
     module.value("$routerRootComponent", "lostPropertyApp");
 
