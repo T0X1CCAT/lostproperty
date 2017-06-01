7ee3415772faa6874d3dfdb48f31a36fcb115133
thanel@redletra.com <thanel@redletra.com>
2017-06-01 21:49:22 +1000

    find, update, delete, located

--- public/app/lost-property-app.component.js
+++ public/app/lost-property-app.component.js
@@ -8,6 +8,7 @@
             {path:"/find", component:"find", name: "Find"},
             {path:"/home", component:"home", name:"Home"},
             {path:"/place", component:"place", name:"Place"},
+            {path:"/place/:id", component:"place", name:"EditPlace"},
             {path:"/list-categories", component:"listCategories", name:"ListCategories"},
             {path:"/add-category", component:"addCategory", name:"AddCategory"},
             {path:"/detail/:id/...", component: "movieDetails", name:"Details"},
--- public/app/place.component.js
+++ public/app/place.component.js
@@ -33,8 +33,82 @@
                         // or server returns response with an error status.
                     });
             };
+            model.deleteItem = function(){
+               // toaster.pop('error', "Confirm", "{template: 'confirmDeleteTmpl.html'}", 15000,null);
+                toaster.pop('warning', "Hi ", "{template: 'confirmDeleteTmpl.html', data: 'MyData'}", 15000, 'templateWithData');
+            };
+            model.deleteItemConfirmed = function(){
+                $http.post(
+                    "/api/deleteItem?id="+model._id,
+                    model,{
+                        headers: {
+                            Authorization: 'Bearer '+ authentication.getToken()
+                        }
+                    }
+                ).then(
+                    function successCallback(response) {
+                        console.log('response', response.data);
+                        if(response.data.status == 'ok'){
+                            toaster.pop('success', "Result", 'Item has been deleted.');
+                            angular.element(document.getElementById('deleteSubmit')).remove();
+                            angular.element(document.getElementById('updateItem')).remove();
+                        }
+                       
+                    }, 
+                    function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+            };
+            
+            model.itemLocated = function(){
+                $http.post(
+                    "/api/itemLocated?id="+model._id,
+                    model,{
+                        headers: {
+                            Authorization: 'Bearer '+ authentication.getToken()
+                        }
+                    }
+                ).then(
+                    function successCallback(response) {
+                        console.log('response', response.data);
+                        if(response.data.status == 'ok'){
+                            toaster.pop('success', "Result", 'Item successfully marked as having been found.');
+                            angular.element(document.getElementById('locatedSubmit')).remove();
+                            model.located = true;
+                        }
+                       
+                    }, 
+                    function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+            };
+            model.updateItem = function(){
+                console.log('update Item');
+                
+                $http.post(
+                    "/api/updateItem",
+                    model,{
+                        headers: {
+                            Authorization: 'Bearer '+ authentication.getToken()
+                        }
+                    }
+                ).then(
+                    function successCallback(response) {
+                        console.log('response', response.data);
+                        if(response.data.status == 'ok'){
+                            toaster.pop('success', "Result", 'Item Updated.');
+                        }
+                       
+                    }, 
+                    function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+            }
             model.saveItem = function(){
-            console.log('save Item');
+                console.log('save Item');
                 
                 $http.post(
                     "/api/place",
@@ -59,9 +133,44 @@
                     
             }       
 
-            this.$onInit = function () {
+            model.$onInit = function () {
                 model.loadCategories();
+
+
             };
+
+            model.$routerOnActivate = function(next) {
+              // Get the id if present
+              var id = next.params.id;
+              if (id){
+                //load up the item from the db
+                $http.get(
+                    "/api/place?id="+id,{
+                        headers: {
+                                Authorization: 'Bearer '+ authentication.getToken()
+                            }
+                    }
+                        
+                    ).then(
+                        function successCallback(response) {
+                            //console.log('response', response);
+                            model._id = response.data._id;
+                            model.itemDate = new Date(response.data.itemDate);
+                            model.itemLocation = response.data.itemLocation;
+                            model.itemDescription = response.data.itemDescription;
+                            model.itemLostOrFound = response.data.itemLostOrFound;
+                            model.itemName = response.data.itemName;
+                            model.itemTime = response.data.itemTime;
+                            model.itemCategory = response.data.itemCategory;
+                            model.located= response.data.located;
+                        }, 
+                        function errorCallback(response) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                        });
+                }
+             
+            }
 
         }]
     });
